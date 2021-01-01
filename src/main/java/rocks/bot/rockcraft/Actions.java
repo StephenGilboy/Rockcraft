611cb0127854413f1b8198548efad5755586041b
Stephen Gilboy <sgilboy@meredith.com>
2020-12-31 19:57:20 -0600

    Github action

--- src/main/java/rocks/bot/rockcraft/Actions.java
+++ src/main/java/rocks/bot/rockcraft/Actions.java
@@ -6,9 +6,7 @@ import org.bukkit.entity.Player;
 public class Actions {
     public static void LightningStrike(Player player, Material material) {
         Material item = player.getInventory().getItemInMainHand().getType();
-        player.sendMessage("You have a " + item + " in your main hand.");
         if (item == material) {
-            player.sendMessage("Flash!");
             player.getWorld().strikeLightning(player.getTargetBlock(null, 200).getLocation());
         }
     }
